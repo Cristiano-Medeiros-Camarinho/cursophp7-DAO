43d2c83c0692f8abdfa70cec084b41d0140cfbb6
Cristiano-Medeiros-Camarinho <crislive.com.pt@gmail.com>
2018-07-24 21:50:21 -0300

    Criamos a classe usuario

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 
 require_once ("config.php");
 
-$sql = new Sql();
+/*$sql = new Sql();
 
 $produtos = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($produtos);
+echo json_encode($produtos);*/
+
+$root = new Usuario();
+
+$root->loadById(3);
+
+echo $root;
 
 
 
