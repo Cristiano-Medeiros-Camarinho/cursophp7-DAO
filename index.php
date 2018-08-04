5dcc1084c1f10223658a1f76f14b78cb27aed50e
Cristiano-Medeiros-Camarinho <crislive.com.pt@gmail.com>
2018-08-04 15:06:25 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -3,18 +3,26 @@
 require_once ("config.php");
 
 /*$sql = new Sql();
-
 $produtos = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($produtos);*/
 
-$root = new Usuario();
-
+//Carrega um usuario pelo id
+/*$root = new Usuario();
 $root->loadById(3);
-
 echo $root;
+*/
 
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+//Carrega uma lista de ususarios buscando pelo login
+//$search = Usuario::search("ro");
+//echo json_encode($search);
 
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "123");
+echo $usuario;
 
  ?>
